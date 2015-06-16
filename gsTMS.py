ba88406a7a08bd280f0821b88df43ccc6b8f8e15
Paul Chater <developer@debian>
2015-06-16 18:38:48 -0400

    Added gsTMS

---
+++ gsTMS.py
@@ -0,0 +1,26 @@
+################################################
+# Tournament Management Software               #
+# version:    0.01-dev                         #
+# author:     Paul "Ninj4" Chater aka PC Hater #
+################################################
+
+from tkinter import *
+
+class gsTMS(Frame):
+
+    def __init__(self, *args, **kwargs):
+        # Insert Main init script here.
+
+    def login(self, *args, **kwargs):
+        # Insert login form here.
+
+    def register(self, *args, **kwargs):
+        # Register form here.
+
+    def main(self, *args, **kwargs):
+        # Main view here.
+
+# Insert Login Form now.
+root = Tk()
+
+root.mainloop()
