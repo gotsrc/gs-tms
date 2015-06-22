69d36ca0287ff963a89d6f8618b63c52a87be4b1
Paul "PC Hater" Chater <p.chater@icloud.com>
2015-06-22 14:04:22 +0100

    updated main and changed supported system list.

--- gsTMS.py
+++ gsTMS.py
@@ -6,22 +6,7 @@
 
 from tkinter import *
 
-class gsTMS(Frame):
-    # def __init__(self, *args, **kwargs):
-        # Insert Main init script here.
+#class gsTMS(Frame):
 
-    #class Login(Frame):
-     #   def __init__(self, *args, **kwargs):
-        # Insert login form here.
-
-    #class Register(Frame):          
-    #def __init__(self, *args, **kwargs):
-        # Register form here.
-
-    #class Main(Frame):
-     #   def __init__(self, *args, **kwargs):
-        # Main view here.
-
-# Insert LoginForm now.
-    root = Tk()
+root = Tk()
 mainloop()
